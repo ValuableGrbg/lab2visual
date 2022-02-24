b0ee9102bcae29a4d751bc631623cb9b6559a9b8
Vadim Maltsev <kanoplanov@mail.ru>
2022-02-24 20:24:53 +0700

    Добавьте файлы проекта.

---
+++ lab2visual/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace lab2visual
+{
+    class Program
+    {
+        static void Main()
+        {
+            RomanNumber n1 = new((ushort)437);
+            RomanNumber n2 = new((ushort)2);
+            Console.WriteLine("437 = " + n1);
+            Console.WriteLine("2 = " + n2);
+            Console.WriteLine("437 + 2 = " + RomanNumber.Add(n1, n2));
+            Console.WriteLine("437 - 2 = " + RomanNumber.Sub(n1, n2));
+            Console.WriteLine("437 * 2 = " + RomanNumber.Mul(n1, n2));
+            Console.WriteLine("437 / 2 = " + RomanNumber.Div(n1, n2));
+            
+            short[] Values = new short[]
+            { 9, 6, 5, 4, 3, 2, 1 };
+            RomanNumber[] romanArray = new RomanNumber[Values.Length];
+            Console.WriteLine("Array:");
+            for (int i = 0; i < Values.Length; ++i)
+            {
+                romanArray[i] = new RomanNumber(((ushort)Values[i]));
+                Console.Write(Values[i] + "(" + romanArray[i].ToString() + ")" + " ");
+            }
+            Console.WriteLine("\nSorted Array:");
+            Array.Sort(romanArray);
+            Array.Sort(Values);
+            for (int i = 0; i < Values.Length; ++i)
+            {
+                Console.Write(Values[i] + "(" + romanArray[i].ToString() + ")" + " ");
+            }
+        }
+    }
+}
